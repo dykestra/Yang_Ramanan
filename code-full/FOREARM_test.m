5fcb4688e08cc59b5560b5e4b629d84b0a7dab42
dykestra <nb1712@potoo01.doc.ic.ac.uk>
2016-04-04 00:40:45 +0100

    results for 18 part 1 mix and 12 mix

--- code-full/FOREARM_test.m
+++ code-full/FOREARM_test.m
@@ -1,16 +1,17 @@
-clc; close all; clear;
+function FOREARM_test(N, mix, sbin)
+
 globals;
 name = 'FOREARM_ROT';
 % --------------------
 % specify model parameters
 % specify mix mixtures per part, for N parts
-N = 18
-mix = 1
+N
+mix
 [K, pa] = get_K_pa(N,mix);
 
 % Spatial resolution of HOG cell, interms of pixel width and hieght
 % The FOREARM dataset contains low-res people, so we use low-res parts
-sbin = 4
+sbin
 % --------------------
 % Prepare training and testing images and part bounding boxes
 % You will need to write custom *_data() functions for your own dataset
@@ -20,14 +21,14 @@ pos = point2box(pos,pa);
 
 % --------------------
 % load model from cache
-cls = [name '_final_' num2str(K')'];
-%cls = [name '_final_' num2str(K(1)) '_' num2str(length(K))];
+%cls = [name '_final_' num2str(K')'];
+cls = [name '_final_' num2str(K(1)) '_' num2str(length(K))];
 load([cachedir cls]);
 
 % --------------------
 % testing phase 1
 % human detection + pose estimation
-suffix = num2str(K')';
+suffix = [num2str(K(1)) '_' num2str(length(K))] ;
 model.thresh = min(model.thresh,-2);
 boxes = testmodel(name,model,test,suffix);
 % additional nms 
@@ -40,4 +41,4 @@ end
 % pose estimation given ground truth human box
 model.thresh = min(model.thresh,-2);
 boxes_gtbox = testmodel_gtbox(name,model,test,suffix);
-
+end
