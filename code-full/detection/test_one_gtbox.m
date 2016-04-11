5d2cefe2bc856afc21789a9a1f6c6bc2aa8ff908
Na K Bassett <lemondice@aol.com>
2016-04-11 14:07:08 +0100

    fix variable name from test to test_one

--- code-full/detection/test_one_gtbox.m
+++ code-full/detection/test_one_gtbox.m
@@ -6,8 +6,8 @@ function [ box ] = test_one_gtbox(name, model,test_one,i)
     fprintf([name ': testing: %d\n'],i);
     im = imread(test_one.im);
     box = detect_fast(im,model,model.thresh);
-    x = test(i).point(:,1);
-    y = test(i).point(:,2);
+    x = test_one(i).point(:,1);
+    y = test_one(i).point(:,2);
     gtbox = [min(x) min(y) max(x) max(y)];
     box = bestoverlap(box,gtbox,0.3);
 end
