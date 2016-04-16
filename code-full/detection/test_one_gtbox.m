ea951b159558045cd635c9e4e960c5cbadcf71b0
Na K Bassett <lemondice@aol.com>
2016-04-16 19:10:28 +0100

    allow for multiple gt boxes per image

--- code-full/detection/test_one_gtbox.m
+++ code-full/detection/test_one_gtbox.m
@@ -1,13 +1,23 @@
 function [ box ] = test_one_gtbox(name, model,test_one,i)
 % 1) Construct ground-truth bounding box
 % 2) Compute all candidates that sufficiently overlap it
-% 3) Return highest scoring one  
+% 3) Return highest scoring one
 
-    im = imread(test_one.im);
-    box = detect_fast(im,model,model.thresh);
+im = imread(test_one.im);
+box = detect_fast(im,model,model.thresh);
+if ~isfield(test_one,'numgt')
     x = test_one.point(:,1);
     y = test_one.point(:,2);
     gtbox = [min(x) min(y) max(x) max(y)];
     box = bestoverlap(box,gtbox,0.3);
+else
+    box0 = box;
+    box = [];
+    for i=1:test_one.numgt
+        x = test_one.point(:,1,i);
+        y = test_one.point(:,2,i);
+        gtbox = [min(x) min(y) max(x) max(y)];
+        box = [box ; bestoverlap(box0,gtbox,0)];
+    end
 end
 
